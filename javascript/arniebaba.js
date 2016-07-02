633c40dbe3aef40c0acf7ea48b27758ca0a39225
voodoo15 <arnold.redoblado@gmail.com>
2016-07-02 15:28:22 -0400

    Adding routing module in angular and created the main and cart pages.  Updated nav bar to reflect the changes.

--- javascript/arniebaba.js
+++ javascript/arniebaba.js
@@ -2,8 +2,19 @@ var arniebaba = angular.module('arniebaba', ['ngRoute']);
 
 arniebaba.config(function ($routeProvider) {
 
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'pages/main.html',
+    controller: 'mainController'
+  })
+
 });
 
 arniebaba.controller('mainController', ['$scope', '$location', function($scope, $location) {
+
+}]);
+
+arniebaba.controller('cartController', ['$scope', '$location', function($scope, $location) {
 
 }]);
